6186bdd483ba5bcadada3aea588e6c1cc76197f7
Sai Chittella <saichittella30@gmail.com>
2022-10-11 08:51:16 -0700

    Update

--- chess.js
+++ chess.js
@@ -379,43 +379,11 @@ function piecesRules() {
             findSlopePawns(boardPieces[piecesArr[i]]); 
         }); 
     } 
+    document.querySelector('#statsButton').addEventListener('click', () => {
+        document.querySelector('#popup-1').classList.toggle('active');
+    });
     
-    document.querySelector('#statsButton').addEventListener('click', modalThings());
 }
-
-const openModalButtons = document.querySelectorAll('[data-modal-target]')
-const closeModalButtons = document.querySelectorAll('[data-close-button]')
-const overlay = document.getElementById('overlay')
-function modalThings() {
-    openModalButtons.forEach(button => {
-        button.addEventListener('click', () => {
-          const modal = document.querySelector(button.dataset.modalTarget)
-          openModal(modal)
-        })
-      })
-      
-      overlay.addEventListener('click', () => {
-        const modals = document.querySelectorAll('.modal.active')
-        modals.forEach(modal => {
-          closeModal(modal)
-        })
-      })
-      
-      closeModalButtons.forEach(button => {
-        button.addEventListener('click', () => {
-          const modal = button.closest('.modal')
-          closeModal(modal)
-        })
-      })
-      
-      function openModal(modal) {
-        if (modal == null) return
-        modal.classList.add('active')
-        overlay.classList.add('active')
-      }
-}
-
-
 
 
 piecesRules();
